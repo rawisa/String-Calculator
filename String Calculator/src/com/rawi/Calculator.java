43b30a303b1823f711b4590843c5c66272342fd7
rawisa <rawi.s21@gmail.com>
2013-12-10 19:53:50 +0200

    Green : Two numbers works

--- String Calculator/src/com/rawi/Calculator.java
+++ String Calculator/src/com/rawi/Calculator.java
@@ -2,8 +2,12 @@ package com.rawi;
 
 public class Calculator {
 	public int add(String number){
-		if(number.isEmpty())
+		if(number.isEmpty()){
 			return 0;
+		}else if(number.contains(",")){
+			String[] numbers = number.split(",");
+			return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+		}
 		else{
 			return Integer.parseInt(number);
 		}
